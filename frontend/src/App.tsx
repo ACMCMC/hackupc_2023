517f8f372686742f51ebdb97247eda96e69e740f
ZacharyB1 <zachary.burda@gmail.com>
2023-05-13 15:19:22 +0200

    commened bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 function App() {
