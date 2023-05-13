7ec44dda5929288b7d3ba8aa43a1fd43fe0a3937
ZacharyB1 <zachary.burda@gmail.com>
2023-05-13 17:01:31 +0200

    user to profile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginButton from './LoginButton';
-import User from './User';
+import Profile from './Profile';
 import LogOut from './LogOut';
 import logo from './logo.svg';
 import './App.css';
@@ -31,7 +31,7 @@ function App() {
         </a>
         <LoginButton />
         <LogOut />
-        <User />
+        <Profile />
       </header>
     </div>
   );
