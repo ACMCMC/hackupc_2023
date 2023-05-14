a1ee77a2c6dcf96640a55731336c69f658e7e9f9
ZacharyB1 <zachary.burda@gmail.com>
2023-05-14 05:22:58 +0200

    add authors and <p>

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -24,18 +24,19 @@ export const AboutUs = () => {
             <Typography variant="h4" component="h4" marginBottom={4}>
               Let MiKasa think on behalf of you!
             </Typography>
-            <Typography variant="h6" component="h6" marginBottom={8}>
-              <h1 style = {{ "padding": "5%"}}>Why are we the best way to find your next home?  </h1> 
-              <p style = {{ "padding": "5%"}}>Out frontier AI technology will help you find the best home for you.  </p> 
-              <p style = {{ "padding": "5%"}}>All you need to do is to tell us what you want and we will do the rest!  </p> 
-              <p style = {{ "padding": "5%"}}>Easy to use website with a simple and intuitive interface will make sure you find your next home in no time.  </p> 
-              <p style = {{ "padding": "5%"}}>Just tell us more about yourself, your hobbies and your interests. </p> 
-              <p style = {{ "padding": "5%"}}>After that MiKasa AI will anylise the real estate market and available properties to find a dream house that is suitable for you the most! </p> 
-              <h1 style = {{ "padding": "5%"}}>JAAZ team - HackUPC 2023  </h1> 
-              <p style = {{ "padding": "5%"}}>@AOrps</p>
-              <p style = {{ "padding": "5%"}}>@acmcmc</p>
-              <p style = {{ "padding": "5%"}}>@JoachimStanislaus</p>
-              <p style = {{ "padding": "5%"}}>@ZacharyB1</p>
+            <Typography variant="h6" component="h6" marginBottom={8}  justifyContent="center"  alignItems="center" marginY={{xs: 4, md: 8}}>
+              <h1>Why are we the best way to find your next home?  </h1> 
+              <p>Out frontier AI technology will help you find the best home for you.  </p> 
+              <p>All you need to do is to tell us what you want and we will do the rest!  </p> 
+              <p>Easy to use website with a simple and intuitive interface will make sure you find your next home in no time.  </p> 
+              <p>Just tell us more about yourself, your hobbies and your interests. </p> 
+              <p>After that MiKasa AI will anylise the real estate market and available properties to find a dream house that is suitable for you the most! </p> 
+              <h1>Done by JAAZ team with love</h1> 
+              <p>Team members:</p>
+              <p>@AOrps</p>
+              <p>@acmcmc</p>
+              <p>@JoachimStanislaus</p>
+              <p>@ZacharyB1</p>
 
 
             </Typography>
